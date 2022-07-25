09c405a00777860d9d48974794cb70dfe32a813b
Luis Gustavo <guleite3@gmail.com>
2022-07-25 19:49:13 -0300

    Refatoracao V1

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function teste() {
-  let a = (document.querySelector('main').innerHTML = ` <div class="container">
-  <label class="switch"><span class="switch-text"><p>Dark</p><p>Light</p></span><br><div class="switch-wrapper"><input type="checkbox" name="btnradio" id="btnradio" onChange="options()"><span class="switch-button"></span></div></label>
-  </div>`)
-}
-
 function options() {
-  let cbx = document.querySelector('input')
-  let corpo = document.querySelector('body')
 
-  cbx.checked
-    ? corpo.classList.replace(corpo.className, 'light-blue')
-    : corpo.classList.replace(corpo.className, 'dark-blue')
 }
