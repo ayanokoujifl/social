1b3163b877ef4a408c748cc6ccca5560c2ae8b91
Luis Gustavo <guleite3@gmail.com>
2022-07-29 21:01:01 -0300

    Finalizado V1.2

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function options() {
-
+function mudaCor() {
+  let body = document.querySelector('body')
+  let options = document.querySelector('select')
+  if (options.selectedIndex == 0) {
+    body.classList.replace(body.className, 'light-blue')
+  } else if (options.selectedIndex == 1) {
+    body.classList.replace(body.className, 'dark-blue')
+  } else if (options.selectedIndex == 2) {
+    body.classList.replace(body.className, 'light-pink')
+  } else if (options.selectedIndex == 3) {
+    body.classList.replace(body.className, 'dark-pink')
+  }
 }
